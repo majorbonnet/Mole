73534aca26978263b84ce6ec0e52bcf20137753f
Ben <bmjaspers@gmail.com>
2015-01-15 22:16:33 -0500

    Hello World and initial REPL files

---
+++ prompt.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer) + 1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy) - 1] = '\0';
+	return cpy;
+}
+
+/* Fake add history function */
+void add_history(char* unused) {}
+
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+int main(int argc, char** argv) {
+
+	puts("Lispy Version 0.0.0.0.1");
+	puts("Press Ctrl+c to exit\n");
+	
+	while (1) {
+		
+		char* input = readline("lispy> ");
+		add_history(input);
+		
+		printf("No you're a %s\n", input);
+		free(input);
+	}
+	
+	return 0;
+}
