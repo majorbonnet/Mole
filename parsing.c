dc3d8e6558a672ae05c12b2e77920a26f4a8b386
Ben <bmjaspers@gmail.com>
2015-04-05 22:06:37 -0400

    Update the parsing to allow for floats or ints
    The parser has been updated to allow for either floats or ints, so that I
    could set up the modulo operator for ints. In order to allow this, the
    eval function is now returning a struct that holds the value(s) and a type
    string. There is probably a much better way to handle a lot of this code,
    but I am way out of touch with C best practices.

--- parsing.c
+++ parsing.c
@@ -25,23 +25,34 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
-double eval_op(double, char*, double);
-double eval(mpc_ast_t*);
+typedef struct 
+{
+    char* type;
+    int i;
+    double d;
+} Value;
+
+Value eval_op(Value, char*, Value);
+double eval_float_op(double, char*, double);
+int eval_int_op(int, char*, int);
+Value eval(mpc_ast_t*);
 
 int main(int argc, char** argv) {
-	mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Flt = mpc_new("flt");
+	mpc_parser_t* Integer = mpc_new("integer");
 	mpc_parser_t* Operator = mpc_new("operator");
 	mpc_parser_t* Expr = mpc_new("expr");
 	mpc_parser_t* Lispy = mpc_new("lispy");
 	
 	mpca_lang(MPCA_LANG_DEFAULT,
-		"											                    \
-		number		: /-?[0-9]+([.][0-9]+)?/ ;					            \
-		operator	: '+' | '-' | '*' | '/' ;			\
-		expr		: <number> | '(' <operator> <expr>+ ')' ;           \
-        lispy       : /^/ <operator> <expr>+ /$/ ;                      \
+		"                                               \
+        integer     : /-?[0-9]+/ ;                                       \
+        flt 		: /-?[0-9]+([.][0-9]+)/ ;	        \
+		operator	: '+' | '-' | '*' | '/'  | '%' ;			            \
+        expr		: <flt> | <integer> | '(' <operator> <expr>+ ')' ;           \
+        lispy       : /^/ <operator> <expr>+ /$/ ;                            \
 		",
-		 Number, Operator, Expr, Lispy);
+		 Flt, Integer, Operator, Expr, Lispy);
 
 	puts("Lispy Version 0.0.0.0.1");
 	puts("Press Ctrl+c to exit\n");
@@ -53,8 +64,17 @@ int main(int argc, char** argv) {
 		mpc_result_t r;
         
         if(mpc_parse("<stdin>", input, Lispy, &r)) {
-            double result = eval(r.output);
-            printf("%f\n", result);
+            Value result = eval(r.output);
+            
+            if (strstr(result.type, "flt"))
+            {
+                printf("%f\n", result.d);
+            }
+            else
+            {
+                printf("%d\n", result.i);
+            }
+            
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
@@ -64,28 +84,90 @@ int main(int argc, char** argv) {
 		free(input);
 	}
 	
-    mpc_cleanup(4, Number, Operator, Expr, Lispy);
+    mpc_cleanup(4, Flt, Integer, Operator, Expr, Lispy);
     
 	return 0;
 }
 
-double eval_op(double x, char* op, double y) {
+Value eval_op(Value x, char* op, Value y) {
+    Value val;
+    
+    // there has to be a more efficient way to structure this
+    // I need to read up on my C again.
+    if (strstr(x.type, "integer") && strstr(y.type, "integer")) 
+    {
+        val.type = "integer\0";
+        val.i = eval_int_op(x.i, op, y.i);
+        
+        return val;
+    }
+    
+    if (strstr(x.type, "flt") && strstr(y.type, "integer"))
+    {
+        val.type = "flt\0";
+        val.d = eval_float_op(x.d, op, (double)y.i);
+        return val;
+    } 
+    
+    if (strstr(x.type, "integer") && strstr(y.type, "flt"))
+    {
+        val.type = "flt\0";
+        val.d = eval_float_op((double)x.i, op, y.d);
+        return val;
+    } 
+    
+    if (strstr(x.type, "flt") && strstr(y.type, "flt"))
+    {
+        val.type = "flt\0";
+        val.d = eval_float_op(x.d, op, y.d);
+        return val;
+    } 
+
+    // default return value
+    // should really throw an error if we hit this point    
+    val.type = "integer\0";
+    val.i = 0;
+    return val;
+}
+
+int eval_int_op(int x, char* op, int y)
+{
     if (strcmp(op, "+") == 0) { return x + y; }
     if (strcmp(op, "-") == 0) { return x - y; }
     if (strcmp(op, "*") == 0) { return x * y; }
     if (strcmp(op, "/") == 0) { return x / y; }
-    
+    if (strcmp(op, "%") == 0) { return x % y; }
+      
     return 0;
 }
 
-double eval(mpc_ast_t* t) {
-    if (strstr(t->tag, "number")) {
-        return atof(t-> contents);
+double eval_float_op(double x, char* op, double y)
+{
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+     
+    return 0;
+}
+
+Value eval(mpc_ast_t* t) {
+    Value val;
+    if (strstr(t->tag, "integer")) {
+        val.type = "integer\0";
+        val.i = atoi(t-> contents);
+        return val;
+    }
+    
+    if (strstr(t->tag, "flt")) {
+        val.type = "flt\0";
+        val.d = atof(t-> contents);
+        return val;
     }
     
     char* op = t->children[1]->contents;
     
-    double x = eval(t->children[2]);
+    Value x = eval(t->children[2]);
     
     int i = 3;
     
